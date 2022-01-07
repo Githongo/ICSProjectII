31cede5df03c0fa6f840643372d935a97c9ead28
Jeffrey King'ori <jeffreygithongo@gmail.com>
2022-01-07 14:21:41 +0300

    Adds Tweepy fetching and connection to DB

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,8 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
+    path('analyse/', views.analyse, name='analyse'),
     path('classify/', views.classify, name='classify'),
     path('result/', views.result, name='result'),
+
 
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 from mainapp.apps import MainappConfig
 
+import tweepy
+
 
 def index(request):
     
@@ -17,6 +19,27 @@ def home(request):
         "title" : "Dashboard",
     }
     return render(request, 'pages/home.html', context)
+
+def analyse(request):
+
+    if request.method == 'GET':
+        context = {
+            "title": "Analyse Tweets",
+        }
+        return render(request, 'pages/analyse.html', context)
+    elif request.method == 'POST':
+        topics = request.POST['topics']
+        topics = topics.replace(" ", "")
+        topics = topics.replace(",", " OR ")
+
+        tweets = fetchTweets('a')
+
+        context = {
+            "title": "Analysed Tweets",
+            "content": tweets,
+        }
+        return render(request, 'pages/analyse.html', context)
+
 
 def classify(request):
     context = {
@@ -33,3 +56,27 @@ def result(request):
         "text": prediction,
     }
     return render(request, 'pages/classify.html', context)
+
+def fetchTweets(topics):
+    twitter_auth_keys = {
+        "consumer_key": "mHz1VOi1NT34C6TPDlgCrJVQh",
+        "consumer_secret": "3KTJrd68bwPsx0lvEfWlV5sKC41UJj16tWn4aUjFcIeKvHqjP2",
+        "access_token": "998973769694810113-LYxfLnmSfv2RIDGxOwDp0DKI7ocruVX",
+        "access_token_secret": "cI3AZTXJHGwWDkVKwfNGqMs50qjDyNbsbsD9bdfLRHkXG"
+    }
+
+    auth = tweepy.OAuthHandler(
+        twitter_auth_keys['consumer_key'],
+        twitter_auth_keys['consumer_secret']
+    )
+    auth.set_access_token(
+        twitter_auth_keys['access_token'],
+        twitter_auth_keys['access_token_secret']
+    )
+    api = tweepy.API(auth)
+    
+    corona_tweets = tweepy.Cursor(api.search_tweets, q="iPhone OR iOS OR Apple -filter:retweets",lang = "en", show_user = True,tweet_mode="extended").items(50)
+    corona_tweets_list = [[tweet.created_at, tweet.place, tweet.user.name, tweet.full_text] for tweet in corona_tweets]
+    
+
+    return corona_tweets_list[0] #Tweet text at [3]
